56a8651d864b3e9abb11326b6d7b23422cf9a6c1
hoainv-fpt <hoainv@fsoft.com.vn>
2018-09-06 16:16:11 +0700

    Add unit test: - Base configs. - Add TestConfig. - Unit test for utils.py.

---
+++ tests/test_utils.py
@@ -0,0 +1,38 @@
+import unittest
+from shopping_app.utils import *
+from shopping_app.models import *
+
+
+class TestUtils(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_convert_json_to_object(self):
+        passed_object = Item()
+        payload = '{"name": "test_name"}'
+
+        expected = Item()
+        expected.name = "test_name"
+
+        actual = convert_json_to_object(passed_object, payload)
+        self.assertEqual(expected.name, actual.name)
+
+    def test_convert_json_to_object_none(self):
+        passed_object = Item()
+        payload = None
+
+        actual = convert_json_to_object(passed_object, payload)
+        self.assertEqual(actual, None)
+
+    def test_group_by_a_list(self):
+        item_ids = [1, 3, 2, 3, 3]
+        expected = {1: 1, 2: 1, 3: 3}
+
+        actual = group_by_a_list(item_ids)
+
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
