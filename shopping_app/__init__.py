3bdc484a289b377cec0f6cefd01462e32775bb6c
hoainv-fpt <hoainv@fsoft.com.vn>
2018-09-06 12:22:56 +0700

    Add logging module.

--- shopping_app/__init__.py
+++ shopping_app/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import logging.config
+
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -7,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from config import Config
+from shopping_app import logging_config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,6 +19,12 @@ migrate = Migrate(app, db)
 engine = db.engine
 Session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
+
+
+if logging_config.LOGGING:
+    logging.config.dictConfig(logging_config.LOGGING)
+else:
+    logging.basicConfig(level=logging.INFO)
 
 
 @app.teardown_appcontext
---
+++ shopping_app/logging_config.py
@@ -0,0 +1,41 @@
+import os
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'custom': {
+            'format': 'COS %(asctime)s %(levelname)s %(name)s (%(lineno)d) %(message)s',
+            'datefmt': '%Y/%m/%d %H:%M:%S',
+        },
+        'simple': {
+            'format': '%(levelname)s|%(asctime)s|%(message)s',
+            'datefmt': '%Y/%m/%d %H:%M:%S',
+        },
+    }
+    , 'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'custom',
+        }
+    }
+    , 'loggers': {
+        'apps': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'libs': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+        },
+        'core': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+    , "root": {
+        "level": "INFO",
+        "handlers": ["console"]
+    }
+}
