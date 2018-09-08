d93ffe4658c496423a600ef01108e649db28a725
hoainv-fpt <hoainv@fsoft.com.vn>
2018-09-08 20:22:27 +0700

    - Refactor code structure. - Add database module. - Add test config. - Update handler logic.

--- config.py
+++ config.py
@@ -11,3 +11,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
                               or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig:
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') \
+                              or 'sqlite:///' + os.path.join(basedir, 'test_app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
