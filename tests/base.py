083b20a69c434af11bd8c3f35673ff4d0a40b2fc
hoainv-fpt <hoainv@fsoft.com.vn>
2018-09-08 20:55:30 +0700

    Add unit test for request_handler.

--- tests/base.py
+++ tests/base.py
@@ -1,33 +1,24 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_testing import TestCase
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session, sessionmaker
+import unittest
 
-from shopping_app import db
-from flask import Flask
+from flask_testing import TestCase
+
+from shopping_app import app, db, create_app
 
 
 class BaseTestCase(TestCase):
     """A base test case."""
 
     def create_app(self):
-        app = Flask(__name__)
-        app.config.from_object('config.TestConfig')
-
-        with app.app_context():
-            test_db = SQLAlchemy(app)
-            engine = test_db.engine
-            session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-            base = declarative_base()
-            base.metadata.create_all(bind=engine)
-            session().commit()
-
+        self.app = create_app("TEST")
         return app
 
     def setUp(self):
         db.create_all()
-        db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
+
+
+if __name__ == "__main__":
+    unittest.main()
